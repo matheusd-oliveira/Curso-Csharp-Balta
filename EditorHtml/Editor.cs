eda92d83d6149660ce04ab95b741ecc0abc06cc3
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-06-06 22:14:32 -0300

    Salvando texto em arquivo txt e mostrando o arquivo desejado pelo usuario

--- EditorHtml/Editor.cs
+++ EditorHtml/Editor.cs
@@ -29,6 +29,43 @@ namespace EditorHtml
             Console.WriteLine("-----------");
             Console.WriteLine(" Deseja salvar o arquivo? ");
 
+            // Salvar o arquivo ou Não? Caso salve , mostra na tela o modo visualização. Se não salvar, também mostra
+            var op = char.Parse(Console.ReadLine().ToLower());
+            if (op == 's')
+            {
+                Console.WriteLine("Qual o Caminho para salvar o arquivo? ");
+                var path = Console.ReadLine();
+
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.Write(file);
+                }
+                Console.WriteLine($"Arquivo salvo com sucesso no caminho: {path}");
+                Console.WriteLine("Enter para visualizar o arquivo.");
+                Console.ReadKey();
+                Viewer.Show(file.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Você não salvou o arquivo. No entanto, aqui está ele para visualização...");
+                Thread.Sleep(3000);
+                Viewer.Show(file.ToString());
+            }
+
+        }
+
+        public static void Open()
+        {   
+            Console.Clear();
+            Console.WriteLine("Em qual caminho o arquivo está? ");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                Console.WriteLine(file.ReadToEnd());
+            }
+            Console.ReadKey();
+            Menu.Show();
         }
     }
 }
