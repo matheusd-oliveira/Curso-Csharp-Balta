c553ef534238db6da4f55ad475b166b1b182bbad
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-06-14 17:25:16 -0300

    study about exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exceptions
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            var arr = new int[3];
+
+            for (int index = 0; index < 10; index++)
+            {   
+                // System.IndexOutOfRangeException
+                Console.WriteLine(arr[index]);
+            }
+        }
+    }
+}
