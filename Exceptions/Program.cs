f6643fc1eae3fd850673aa5029339cd35b812807
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-06-14 17:28:49 -0300

    Learn try/catch

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -4,14 +4,23 @@
     class Program
     {
         static void Main()
-        {
+        {   
+            Console.Clear();
             var arr = new int[3];
 
-            for (int index = 0; index < 10; index++)
-            {   
-                // System.IndexOutOfRangeException
-                Console.WriteLine(arr[index]);
+            try
+            {
+                for (int index = 0; index < 10; index++)
+                {
+                    // System.IndexOutOfRangeException
+                    Console.WriteLine(arr[index]);
+                }
             }
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Ops, esse array não é tão grande assim...");
+            }
+
         }
     }
 }
