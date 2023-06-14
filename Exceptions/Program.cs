17c860c53a1851a51315b2b1e39523cce00b1bde
Matheus Oliveira <matheussilvaolveira@gmail.com>
2023-06-14 17:57:18 -0300

    Custom Exceptions

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -19,29 +19,47 @@
                 Cadastrar("");
             }
             catch (IndexOutOfRangeException e)
-            {   
+            {
                 Console.WriteLine(e.InnerException);
                 Console.WriteLine(e.Message);
                 Console.WriteLine("Ops, esse array não é tão grande assim...");
             }
             catch (ArgumentNullException e)
-            {   
+            {
                 Console.WriteLine(e.InnerException);
                 Console.WriteLine(e.Message);
                 Console.WriteLine("Falha ao cadastrar texto.");
             }
+            catch (MinhaException e)
+            {
+                Console.WriteLine(e.InnerException);
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.QuandoAconteceu);
+                Console.WriteLine("Exceção customizada");
+            }
             catch (Exception e)
-            {   
+            {
                 Console.WriteLine(e.InnerException);
                 Console.WriteLine(e.Message);
                 Console.WriteLine("Ops, algo deu errado!");
             }
 
         }
-        private  static void Cadastrar(string texto)
+        private static void Cadastrar(string texto)
         {
-            if(string.IsNullOrEmpty(texto))
-                throw new ArgumentNullException("O texto não pode ser nulo ou vazio...");
+            if (string.IsNullOrEmpty(texto))
+                throw new MinhaException(DateTime.Now);
+        }
+
+        public class MinhaException : Exception
+        {
+
+            public MinhaException(DateTime date)
+            {
+                QuandoAconteceu = date;
+            }
+            public DateTime QuandoAconteceu { get; set; }
+
         }
     }
 }
